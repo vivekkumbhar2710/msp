c8851cf6839c390942642ed87602fb748d4a4178
vivekkumbhar2710 <vivekkumbhar27102001@gmail.com>
2024-02-19 18:11:07 +0530

    vivek kumbhar 06.10

---
+++ ms_production/ms_production/doctype/job_work_receipt_bom/job_work_receipt_bom.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Abhishek Chougule and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Job Work Receipt BOM', {
+	// refresh: function(frm) {
+
+	// }
+});
